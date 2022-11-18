a24c2d118f5231b5084473132b5c2492e76875b8
Sarah B <73038204+srhbrwr@users.noreply.github.com>
2022-11-18 11:30:12 -0600

    imported axios

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -1,13 +1,22 @@
 import React, { useState } from "react";
+import axios from "axios";
 
 import "./Dictionary.css";
 
 export default function Dictionary() {
     let [keyword, setKeyword] = useState(null);
 
+    function handleResponse(response) {
+        console.log(response.data[0]);
+    }
+
     function search(event) {
-        event.preventDefault();
-        alert(`Searching for ${keyword} definition`);
+      event.preventDefault();
+
+      // Documentation: https://dictionaryapi.dev/
+      
+      let apiUrl = `https://api.dictionaryapi.dev/api/v2/entries/en/${keyword}`;
+      axios.get(apiUrl).then(handleResponse);
     }
 
     function handleKeyword(event) {
