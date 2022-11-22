a2304e1e1d870a5a8bb7f2519889c430d1b7b932
Sarah B <73038204+srhbrwr@users.noreply.github.com>
2022-11-22 09:17:23 -0600

    Phonetic CSS

--- src/Meaning.js
+++ src/Meaning.js
@@ -1,26 +1,22 @@
 import React from "react";
 
 import Synonyms from "./Synonyms";
+import "./Meaning.css";
 
 export default function Meaning(props) {
-    console.log(props.meaning);
-    return (
-      <div className="Meaning">
-        <h3>{props.meaning.partOfSpeech}</h3>
-        {props.meaning.definitions.map(function(definition, index) {
-            return (
-              <div key={index}>
-                <p>
-                  <strong>Definition: </strong>{definition.definition}
-                  <br />
-                  <em>{definition.example}</em>
-                  <br />
-                  <Synonyms synonyms={definition.synonyms} />
-                </p>
-              </div>
-            );         
-        })}
+  return (
+    <div className="Meaning">
+      <h3>{props.meaning.partOfSpeech}</h3>
+      {props.meaning.definitions.map(function (definition, index) {
+        return (
+          <div key={index}>
+            <div className="definition">{definition.definition}</div>
 
-      </div>
-    );
+            <div className="example">{definition.example}</div>
+            <Synonyms synonyms={definition.synonyms} />
+          </div>
+        );
+      })}
+    </div>
+  );
 }
