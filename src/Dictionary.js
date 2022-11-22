bc0aeb2d33dd2e7d007fc32c246370923dda6343
Sarah B <73038204+srhbrwr@users.noreply.github.com>
2022-11-22 09:36:51 -0600

    Fixed error

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -1,42 +1,70 @@
 import React, { useState } from "react";
 import axios from "axios";
-
 import Results from "./Results";
+import Photos from "./Photos";
 import "./Dictionary.css";
 
-export default function Dictionary() {
-    let [keyword, setKeyword] = useState("");
-    let [results, setResults] = useState(null);
+export default function Dictionary(props) {
+  let [keyword, setKeyword] = useState(props.defaultKeyword);
+  let [results, setResults] = useState(null);
+  let [loaded, setLoaded] = useState(false);
+  let [photos, setPhotos] = useState(null);
 
-    function handleResponse(response) {
-        setResults(response.data[0]);
-    }
+  function handleDictionResponse(response) {
+    setResults(response.data[0]);
+  }
 
-    function search(event) {
-      event.preventDefault();
+  function handlePexelsResponse(response) {
+    setPhotos(response.data.photos);
+  }
 
-      // Documentation: https://dictionaryapi.dev/
+  function search() {
+    // documentation: https://dictionaryapi.dev/e
+    let apiUrl = `https://api.dictionaryapi.dev/api/v2/entries/en_US/${keyword}`;
+    axios.get(apiUrl).then(handleDictionResponse);
 
-      let apiUrl = `https://api.dictionaryapi.dev/api/v2/entries/en/${keyword}`;
-      axios.get(apiUrl).then(handleResponse);
-    }
+    let pexelsApiKey =
+      "563492ad6f9170000100000154ba544efe954dc99e85f90ea9718c43";
+    let pexelsApiUrl = `https://api.pexels.com/v1/search?query=${keyword}&per_page=9`;
+    let headers = { Authorization: `Bearer ${pexelsApiKey}` };
+    axios.get(pexelsApiUrl, { headers: headers }).then(handlePexelsResponse);
+  }
 
-    function handleKeywordChange(event) {
-        setKeyword(event.target.value);
-    }
+  function handleSubmit(event) {
+    event.preventDefault();
+    search();
+  }
 
+  function handleKeywordChange(event) {
+    setKeyword(event.target.value);
+  }
 
+  function load() {
+    setLoaded(true);
+    search();
+  }
+
+  if (loaded) {
     return (
       <div className="Dictionary">
         <section>
-          <form onSubmit={search}>
-            <input type="search" onChange={handleKeywordChange} />
+          <form onSubmit={handleSubmit}>
+            <input
+              type="search"
+              onChange={handleKeywordChange}
+              defaultValue={props.defaultKeyword}
+            />
           </form>
-          <div className="hint text-left">
-            Suggested words: sunset, wine, yoga, plant...
+          <div className="hint">
+            suggested words: sunset, wine, yoga, plant...
           </div>
         </section>
         <Results results={results} />
+        <Photos photos={photos} />
       </div>
     );
+  } else {
+    load();
+    return "Loading";
+  }
 }
